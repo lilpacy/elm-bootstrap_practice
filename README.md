59cf1b5b60dce40ff1d23e23c50400bfadea20b1
pacy <tomohiro_seto@fringe81.com>
2020-01-17 15:36:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# elm-parcel-min
+
+## How to setup
+
+```shell:
+git clone git@github.com:lilpacy/elm-spa-trivial.git
+cd elm-spa-trivial
+npm run setup
+```
+
+## How to run dev-server
+```shell:
+npm run dev
+```
+
---
+++ src/Main.elm
@@ -0,0 +1,198 @@
+port module Main exposing (Msg(..), main, update, view)
+
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
+import Browser
+import Html exposing (Html, a, div, h1, h3, text)
+import Html.Attributes exposing (class, href)
+import Html.Events exposing (preventDefaultOn)
+import Json.Decode as D
+
+
+port onUrlChange : (() -> msg) -> Sub msg
+
+
+port getLocation : () -> Cmd msg
+
+
+port gotLocation : (String -> msg) -> Sub msg
+
+
+port pushUrl : String -> Cmd msg
+
+
+port setOffsets : () -> Cmd msg
+
+
+type alias Flags =
+    String
+
+
+main : Program Flags Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+subscriptions _ =
+    Sub.batch
+        [ onUrlChange UrlChanged
+        , gotLocation GotLocation
+        ]
+
+
+type Model
+    = Top
+    | Polano
+    | MilkyTrain
+    | RainWind
+    | NotFound
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( locationHrefToModel flags
+    , Cmd.none
+    )
+
+
+type Msg
+    = UrlChanged ()
+    | GotLocation String
+    | Clicked String
+    | NoOp
+
+
+locationHrefToModel : String -> Model
+locationHrefToModel here =
+    if here == "http://localhost:1234/" then
+        Top
+
+    else if here == "http://localhost:1234/polano-square" then
+        Polano
+
+    else if here == "http://localhost:1234/milky-train" then
+        MilkyTrain
+
+    else if here == "http://localhost:1234/rain-wind" then
+        RainWind
+
+    else
+        NotFound
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UrlChanged _ ->
+            ( model
+            , getLocation ()
+            )
+
+        Clicked url ->
+            ( model
+            , pushUrl url
+            )
+
+        GotLocation url ->
+            ( locationHrefToModel url
+            , setOffsets ()
+            )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+type alias Page =
+    { title : String, phrase : String }
+
+
+topPage : Html Msg
+topPage =
+    div
+        [ class "background top" ]
+        [ h1 [] [ text "宮沢賢治" ]
+        , link (Clicked "http://localhost:1234/polano-square") [ href "" ] [ text "ポラーノの広場" ]
+        , link (Clicked "http://localhost:1234/milky-train") [ href "" ] [ text "銀河鉄道の夜" ]
+        , link (Clicked "http://localhost:1234/rain-wind") [ href "" ] [ text "雨ニモマケズ風ニモマケズ" ]
+        ]
+
+
+polanoSquarePage : Page
+polanoSquarePage =
+    { title = "ポラーノの広場"
+    , phrase = "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。"
+    }
+
+
+milkyTrainPage : Page
+milkyTrainPage =
+    { title = "銀河鉄道の夜"
+    , phrase = "カムパネルラ、また僕たち二人きりになったねえ、どこまでもどこまでも一緒に行こう。僕はもうあのさそりのようにほんとうにみんなの幸さいわいのためならば僕のからだなんか百ぺん灼やいてもかまわない。"
+    }
+
+
+rainWindPage : Page
+rainWindPage =
+    { title = "雨ニモマケズ風ニモマケズ"
+    , phrase = "雨ニモマケズ風ニモマケズ雪ニモ夏ノ暑サニモマケヌ丈夫ナカラダヲモチ慾ハナク決シテ瞋ラズイツモシヅカニワラッテヰル"
+    }
+
+
+view : Model -> Html Msg
+view model =
+    case model of
+        NotFound ->
+            div [] [ text "404 not found" ]
+
+        Top ->
+            topPage
+
+        Polano ->
+            viewPage polanoSquarePage
+
+        MilkyTrain ->
+            viewPage milkyTrainPage
+
+        RainWind ->
+            viewPage rainWindPage
+
+
+viewPage : Page -> Html Msg
+viewPage page =
+    let
+        outer =
+            page.phrase |> String.repeat 100
+
+        body =
+            Grid.row
+                [ Row.centerXs ]
+                -- Row.centerXs creates a Row.Option
+                -- The second argument to row is a list of Grid.Column items
+                [ Grid.col
+                    [ Col.xs12
+                    , Col.attrs [ class "custom-class" ] -- <module>.attrs function, creates an option to specify a list of custom Elm Html attributes.
+                    ]
+                    [ text "Some full width column." ]
+                ]
+    in
+    Grid.container []
+        [ CDN.stylesheet
+        , body
+        ]
+
+
+link : msg -> List (Html.Attribute msg) -> List (Html msg) -> Html msg
+link msg attrs children =
+    a (preventDefaultOn "click" (D.map alwaysPreventDefault (D.succeed msg)) :: attrs) children
+
+
+alwaysPreventDefault : msg -> ( msg, Bool )
+alwaysPreventDefault msg =
+    ( msg, True )
